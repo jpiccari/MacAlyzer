ed6942b32ccb11caa955aa8f8b52a12d6c8097ad
Joshua Piccari <joshua.piccari@gmail.com>
2011-05-28 22:47:22 -0700

    Initial Commit

---
+++ MacAlyzer/MACapture.h
@@ -0,0 +1,15 @@
+//
+//  MACapture.h
+//  MacAlyzer
+//
+//  Created by Joshua Piccari on 5/28/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MACapture : NSDocument {
+@private
+}
+
+@end
---
+++ MacAlyzer/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  MacAlyzer
+//
+//  Created by Joshua Piccari on 5/28/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+	return NSApplicationMain(argc, (const char **)argv);
+}
